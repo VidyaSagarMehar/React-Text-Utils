f57e5252b3c58b836c27bf9502c6e1394f476aed
VidyaSagarMehar <vidyasagark890@gmail.com>
2022-12-03 22:03:00 +0530

    initial commit

---
+++ src/components/TextForm.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+
+export default function TextForm(props) {
+	const handleUpClick = () => {
+		// console.log('Uppercase was clicked' + text);
+		let newText = text.toUpperCase();
+		setText(newText);
+	};
+	const handleLowerClick = () => {
+		// console.log('Uppercase was clicked' + text);
+		let newText = text.toLocaleLowerCase();
+		setText(newText);
+	};
+
+	const handleClearClick = () => {
+		setText('');
+	};
+	const handleReverseClick = () => {
+		let reverseText = text.split('').reverse().join('');
+		setText(reverseText);
+	};
+	const handleOnChange = (event) => {
+		// console.log('Uppercase was clicked');
+		setText(event.target.value);
+	};
+
+	const [text, setText] = useState('Type here');
+	// text = 'new text' // Wrong way
+	// setText('new text')// Correct way to change the state
+
+	return (
+		<>
+			<div className="mb-3 container">
+				<h1> {props.heading}</h1>
+				<textarea
+					className="form-control"
+					id="exampleFormControlTextarea1"
+					rows="10"
+					value={text}
+					onChange={handleOnChange}
+				></textarea>
+				<button className="btn btn-primary mx-1 my-2" onClick={handleUpClick}>
+					Conver to Uppercase
+				</button>
+				<button
+					className="btn btn-primary mx-1 my-2"
+					onClick={handleLowerClick}
+				>
+					Conver to Lowercase
+				</button>
+				<button
+					className="btn btn-primary mx-1 my-2"
+					onClick={handleClearClick}
+				>
+					Clear TextArea
+				</button>
+				<button
+					className="btn btn-primary mx-1 my-2"
+					onClick={handleReverseClick}
+				>
+					Reverse Text
+				</button>
+			</div>
+			<div className="container my-3">
+				<h3>Your text summary</h3>
+				<p>
+					{text.split(' ').length} word and {text.length} characters
+				</p>
+				<p>{0.008 * text.split(' ').length} Minutes read</p>
+
+				<h3>Preview</h3>
+				<p>{text}</p>
+			</div>
+		</>
+	);
+}
